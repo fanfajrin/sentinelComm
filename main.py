1f2934fd05fe869fe15aeb3c8ec45e10671d8a58
isfan dev <devjugadev@gmail.com>
2025-06-29 18:35:39 +0700

    try granite instruct

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+import replicate
 import pandas as pd
+import json
+from dotenv import load_dotenv
+import os
 
-print('hello world')
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY", "no_api_key_detected")
+
+input = {
+    "prompt": "why llm is usefull? give me answer in one sentence"
+}
+
+for event in replicate.stream(
+    "ibm-granite/granite-3.3-8b-instruct",
+    input=input
+):
+    print(event, end="")
