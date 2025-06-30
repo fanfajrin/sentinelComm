d5cfd4ec38aa22ed0b755cf81f56284df6692a13
isfan dev <devjugadev@gmail.com>
2025-06-30 11:22:22 +0700

    add parameter for granite

--- main.py
+++ main.py
@@ -9,7 +9,14 @@ load_dotenv()
 API_KEY = os.getenv("API_KEY", "no_api_key_detected")
 
 input = {
-    "prompt": "why llm is usefull? give me answer in one sentence"
+    "top_k": 50,
+    "top_p": 0.9,
+    "prompt": "why llm is usefull? give me answer in one sentence",
+    "max_tokens": 512,
+    "min_tokens": 0,
+    "temperature": 0.6,
+    "presence_penalty": 0,
+    "frequency_penalty": 0
 }
 
 for event in replicate.stream(
